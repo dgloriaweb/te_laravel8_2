8e14cc847ad3bd712bb84abefd9120bf213f3e00
unknown <dgloria.web@gmail.com>
2021-01-26 06:18:16 +0000

    added images

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,42 +8,36 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                You haven't got a job that matches more than 40%, is it because you didn't provide enough data? Based on that I
-			cannot give you recommendation for a future role. Please scroll down and fill in more data to continue.
+                You haven't got a job that matches more than 40%, is it because you didn't provide enough data? Based on
+                that I
+                cannot give you recommendation for a future career. Please scroll down and fill in more data to
+                continue.
 
-            <br/>
-            OR
-            <br/>
+                <br />
+                OR
+                <br />
 
-            <div class="card">
-                <h2>Recommended jobs</h2>
-                <br>
-                
-                    <div class="card-text">
-                        <div class="row">
-                            <div class="col-12 col-md-5">
-                                <h5><a href="#">Potter</a></h5>
-                            </div>
-                            <div class="col-12 col-md-7">
-                                <div class="progress progress_home" style="height:1.5rem;">
-                                
-                                    
-                                    <div class="progress-bar progress-bar-striped" role="progressbar" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100" style="width:65%">
-                                        <span class="sr-only">65% Complete</span>
-                                    </div>
-                                    <span class="progress-completed">&nbsp;65%</span>
-                                </div>
-                                <div class="text-blue-700 hover:text-purple-800 underline"><a href="{{ route('user_settings') }}">Go to user settings</a></div>
-                                
-                            </div>
-                        </div>
-                        <br>
+                <div class="p6">
+                    <h2>Recommended jobs</h2>
+                    <br>
+
+                    <div class="p-6">
+                        <h5><a href="#">Potter</a></h5>
+
                     </div>
-    
-    
-               
+                    <div class="text-blue-700 hover:text-purple-800 underline">
+                        <a href="{{ route('user_settings') }}">Go to user settings</a>
+                    </div>
+
+                </div>
             </div>
-            </div>
+            <br>
         </div>
+
+
+
+    </div>
+    </div>
+    </div>
     </div>
 </x-app-layout>
