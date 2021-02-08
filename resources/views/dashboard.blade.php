5d9b8340aabf16c6ef0e42d4ca959c94392dd1e6
unknown <dgloria.web@gmail.com>
2021-02-08 15:35:52 +0000

    frontend dynamic data show

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,13 +4,15 @@
     </x-slot>
 
     {{-- personal results --}}
-    
+
     Recommended jobs
     <div class="grid grid-cols-12 p-1 border-b-2">
         @foreach ($all_job_match_rates as $all_job_match_rate)
-      
+        @php
+        $id = $all_job_match_rate['id']
+        @endphp
         <div class="col-span-3 border-b-2">
-            <a href="#"  onclick="openJobContent({{$all_job_match_rate['id']}})">
+            <a href="#" onclick="openJobContent({{$id}})">
                 {{$all_job_match_rate['job_name']}}
             </a>
         </div>
@@ -20,22 +22,36 @@
         <div class="col-span-7 border-b-2 bg-gray-50">
             <div class="jobContent hidden" id="job{{$all_job_match_rate['id']}}">
                 {{$all_job_match_rate['job_name']}}
-                <br/>
-                habba
+                <br />
+                <div>Location:</div>
+                <div>Workplace or remote or both</div>
+                <div>Preferred working days</div>
+                <div>Workdays</div>
+                <div>Saturday</div>
+                <div>Sunday</div>
+                <div>bank holidays</div>
+                <div>Sat, Sun and Bank holidays only</div>
+                <div>Preferred work hours</div>
+                <div>Normal hours</div>
+                <div>nightshift</div>
+                <div>nightshift only</div>
+                <div>Other shift</div>
+                <div>Other shift only</div>
+                <div>Overtime</div>
             </div>
         </div>
         @endforeach
     </div>
-<script>
-    function openJobContent(jobId) {
-    var x = document.getElementById("job" + jobId);
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
-}
+    <script>
+        function openJobContent(jobId) {
+            var x = document.getElementById("job" + jobId);
+            if (x.style.display === "none") {
+                x.style.display = "block";
+            } else {
+                x.style.display = "none";
+            }
+        }
 
-</script>
+    </script>
 
 </x-app-layout>
